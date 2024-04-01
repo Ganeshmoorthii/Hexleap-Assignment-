ac9628232ebbdb80e50860dc8f44efa60d5fe257
Ganeshmoorthii <mgmoorthi2004@gmail.com>
2024-04-01 23:03:56 +0530

    Your new commit message

--- my-next-project/app/page.tsx
+++ my-next-project/app/page.tsx
@@ -2,14 +2,23 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center gap-5 w-screen h-screen">
-      <div className="flex gap-5">
-        <Link href={"/light"} className="bg-blue-500 p-5 text-2xl">
-          Light
+    <div
+      className="flex items-center justify-center w-screen h-screen bg-cover bg-center"
+      style={{
+        backgroundImage: "url('https://wallpapercave.com/wp/zx1itjA.jpg')",
+      }}
+    >
+      <div className="flex flex-col gap-5">
+        <Link href={"/light"}>
+          <div className="bg-blue-500 p-5 text-2xl rounded-lg cursor-pointer">
+            Light
+          </div>
         </Link>
 
-        <Link href={"/dark"} className="bg-blue-500 p-5 text-2xl">
-          Dark
+        <Link href={"/dark"}>
+          <div className="bg-blue-500 p-5 text-2xl rounded-lg cursor-pointer">
+            Dark
+          </div>
         </Link>
       </div>
     </div>
