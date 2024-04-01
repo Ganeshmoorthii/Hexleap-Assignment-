de4965a0bc5647401378e91dfb6473650d832181
Ganeshmoorthii <mgmoorthi2004@gmail.com>
2024-04-01 22:35:22 +0530

    xdf

---
+++ my-next-project/app/page.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center gap-5 w-screen h-screen">
+      <div className="flex gap-5">
+        <Link href={"/light"} className="bg-blue-500 p-5 text-2xl">
+          Light
+        </Link>
+
+        <Link href={"/dark"} className="bg-blue-500 p-5 text-2xl">
+          Dark
+        </Link>
+      </div>
+    </div>
+  );
+}
